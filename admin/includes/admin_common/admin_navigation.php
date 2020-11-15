3f6e604eacc070ec84e317ed7352951fa9d46fa8
Asad Anik <asadanik@Asads-MacBook-Pro.local>
2020-11-15 08:18:00 +0600

    Admin Category Operations Done

--- admin/includes/admin_common/admin_navigation.php
+++ admin/includes/admin_common/admin_navigation.php
@@ -73,7 +73,7 @@
                         <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i> Posts <i class="fa fa-fw fa-caret-down"></i></a>
                         <ul id="demo" class="collapse">
                             <li>
-                                <a href="#">View All Posts</a>
+                                <a href="posts.php">View All Posts</a>
                             </li>
                             <li>
                                 <a href="#">Dropdown Item</a>
